4a153ec9782fa0cfd6a6eb23b65d084ed8064f3b
nbasu-mirp <nbasu-mirp@mit.edu>
2017-05-31 06:51:13 +0530

    updated one

--- student_grade/grade.cpp
+++ student_grade/grade.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 int main()
 {
-	int 	marks; 
+	int 	marks, A, B, C; 
 	cout<<"Enter marks of the student :  "<<endl;
-	cin>>marks;
-	if (marks > 90){
+	cout<<"Enter cut off of the GRADE A :  "<<endl;
+	cout<<"Enter cut off of the GRADE B :  "<<endl;
+	cout<<"Enter cut off of the GRADE C:  "<<endl;
+	cin>>marks>>A>>B>>C;
+	if (A>B && B>C && marks > A){
 		cout<<"grade A"<<endl;
 	}
-	else if (marks > 50){
+	else if (A>B && B>C && marks > B){
 		cout<<"grade B"<<endl;
 	}
-	else if (marks > 20){
+	else if (A>B && B>C && marks > C){
 		cout<<"grade C"<<endl;
 	}
 	else{ 
-		cout<<"FAIL!!!"<<endl;
+		cout<<"INCORRECT CUTOFFS"<<endl;
 	}
 	return 0;
 }
